c06ddf205783a966bdbaeba3213b97a67b9fa94d
Simon Pagden <128382427+Paggers92@users.noreply.github.com>
2023-06-16 14:37:46 +0100

    Allows region comparion

--- ofsted_market_analysis_app/ofsted_market_analysis_app.py
+++ ofsted_market_analysis_app/ofsted_market_analysis_app.py
@@ -3,11 +3,15 @@ import pandas as pd
 import plotly.express as px
 import matplotlib
 
+
 # Takes "Ofsted Social care providers list for LAs"
 uploaded_files = st.file_uploader('Upload Ofsted social care providers list', accept_multiple_files=False)
 
 if uploaded_files:
-    loaded_files = {uploaded_files.name: pd.read_csv(uploaded_files)}
+    if uploaded_files.name[-4:] == '.csv':
+        loaded_files = {uploaded_files.name: pd.read_csv(uploaded_files)}
+    else:
+        loaded_files = {uploaded_files.name: pd.read_excel(uploaded_files)}
     
     for file in loaded_files.items():
         st.write(file[0]) # Print the key from the dictionary for this file
@@ -31,14 +35,32 @@ if uploaded_files:
     df['Number of settings with this owner'] = df.groupby('Owner ID')['Owner ID'].transform('count')
     #st.dataframe(df)
 
+    # Widgit to select either a single or two regions
+    with st.sidebar:
+        comparison_mode = st.sidebar.radio('Would you like to view a single region or compare two regions?',
+             ('One region', 'Multiple regions')
+             )
+
+
     # Widgits to filter the dataframe
     regions = df['Region'].unique()
-    with st.sidebar:
-        region_option = st.sidebar.selectbox(
+
+    if comparison_mode == 'Multiple regions':
+        with st.sidebar:
+            region_multiple = st.sidebar.multiselect(
+                'Select regions',
+                (regions),
+                default = ([regions[0], regions[1]])
+            )
+        df = df[df['Region'].isin(region_multiple)]
+    else:
+        with st.sidebar:
+            region_option = st.sidebar.selectbox(
             'Select region',
-            (regions)
+            (regions),
+            key = 1
         )
-    df = df[df['Region'] == region_option]
+        df = df[df['Region'] == region_option]
 
     with st.sidebar:
         upper_threshold = st.sidebar.number_input(
@@ -47,22 +69,35 @@ if uploaded_files:
             max_value = 100,
             value = 5
         )
+    # Filter dataframe according to selections
     df = df[df['Number of settings with this owner'] <= upper_threshold]
-    
 
     # Select only certain columns in dataframe
     df = df[['URN',
              'Setting name',
              'Owner name',
              'Local authority',
+             'Region',
              'Number of registered places',
              'Number of settings with this owner']]
 
+    
     # Display dataframe
     st.dataframe(df)
 
+    # Group by number of settings per owener
+    counts = df.groupby(['Number of settings with this owner', 'Region']).count().reset_index()
+    #st.dataframe(counts)
+
     # Create a plot
-    fig = px.histogram(df, x='Number of settings with this owner')
+    fig = px.bar(counts,
+                 x = 'Number of settings with this owner',
+                 y = 'URN',
+                 color = 'Region',
+                 #color_discrete_sequence = ['Red', 'Light Blue'],
+                 barmode = 'group'
+    )
     st.plotly_chart(fig)
+
 
     pass
